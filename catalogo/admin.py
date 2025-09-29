e4c712c1758ce00cbe2631b03c34e10df89d8c33
Magdalena Armstrong <magdalena.a9114@gmail.com>
2025-09-29 15:30:27 -0300

    feat: registrar modelos en admin.py

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Productos, Categorias, Usuarios, Nutricional, Direccion, Roles
+
+# Registrar los modelos para que se muestren en el admin
+admin.site.register(Productos)
+admin.site.register(Categorias)
+admin.site.register(Usuarios)
+admin.site.register(Nutricional)
+admin.site.register(Direccion)
+admin.site.register(Roles)
